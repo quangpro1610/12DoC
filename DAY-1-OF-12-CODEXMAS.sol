4bb9a6d9f6b0cc238c6291a7c739d87e6c708d72
quangpro1610 <quangpro1610@gmail.com>
2023-12-11 23:09:45 +0700

    initial upload code

---
+++ DAY-1-OF-12-CODEXMAS.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract mycindyChristmasSmartContract {
+    function sayMerryChristmas() external pure returns (string memory) {
+        return 'Ho, Ho, Ho, Merry Christmas!';
+    }
+}
