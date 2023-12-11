55429e7e7eabe4d74b7369409edf0bcbb888f06c
quangpro1610 <quangpro1610@gmail.com>
2023-12-11 23:37:53 +0700

    Update contract tx and address after deploy Goerli Testnet

--- DAY-1-OF-12-CODEXMAS.sol
+++ DAY-1-OF-12-CODEXMAS.sol
@@ -8,6 +8,8 @@ contract mycindyChristmasSmartContract {
 }
 
 
-//the deployed contract address: 0xd8b934580fcE35a11B58C6D73aDeE468a2833fa8
+//the deployed contract tx: https://goerli.etherscan.io/tx/0xd46f131fe4e223f151c1b83ac4c1db0db38a9cfe02b693928297e3b3dde4e2b7
+
+//the deployed contract address: 0x32A4d64Bd11a75F7B83AF20917dEEc6076B529d4
 
 //Challenge 1
